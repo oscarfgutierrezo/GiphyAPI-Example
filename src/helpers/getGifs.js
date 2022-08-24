f48475dc15dad24a58e4efd3293ed2460d3023d2
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-08-24 15:47:16 -0500

    Get pagination info in fetch

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,13 +1,16 @@
 export const getGifs = async ( category ) => {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=db6VqtFoSWkixuI0gYmzk9VHdjVLgEWa&q=${category}&limit=10`;
   const resp = await fetch( url );
-  const { data } = await resp.json();
+  const { data, pagination } = await resp.json();
   
   const gifs = data.map( gif => ({
     id: gif.id,
     title: gif.title,
     url: gif.images.downsized_medium.url
   }));
+
+  const totalImages = pagination.count;
+  console.log(gifs)
 
   return gifs;
 }
